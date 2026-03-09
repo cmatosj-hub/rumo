173f7fdc43f8bdfaa4e441f9f851708351875cc6
cmatosj <cmatosj@gmail.com>
2026-03-09 00:05:52 -0300

    ajustes para deploy Render

--- server.js
+++ server.js
@@ -118,8 +118,8 @@ media:diaria
 
 })
 
-app.listen(3000,()=>{
+const PORT = process.env.PORT || 3000;
 
-console.log("RUMO rodando em http://localhost:3000")
-
-})
+app.listen(PORT, ()=>{
+  console.log(`RUMO rodando em http://localhost:${PORT}`)
+});
